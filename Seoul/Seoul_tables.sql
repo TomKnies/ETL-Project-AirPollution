e64fced2d276cc24c048fb85d5c94ed5f18218b5
Tom Knies <tomknies@gmail.com>
2020-05-30 11:43:29 -0500

    so sad

--- Seoul/Seoul_tables.sql
+++ Seoul/Seoul_tables.sql
@@ -1,13 +1,19 @@
 -- create table Seoul (
--- 	Year int,
--- 	SO2 float,
--- 	NO2 float,
--- 	O3 float,
--- 	CO float,
--- 	PM10 float,
--- 	PM25 float);
+-- city varchar (10),
+-- Year int,
+-- SO2 float,
+-- NO2 float,
+-- O3 float,
+-- CO float,
+-- PM10 float,
+-- "pm2.5" float);
+
+-- insert into Seoul (city, year, so2, no2, o3, co, pm10, "pm2.5")
+-- select city, year, so2, no2, o3, co, pm10, pm25 from oldtable;
 	
 -- select * from Seoul
+
+-- select * from oldtable
 
 -- create table Seoul_2017 (
 -- 	Year int,
