56ccedee4ae065c310140f8489173539ef54bff1
Tom Knies <tomknies@gmail.com>
2020-05-30 13:21:33 -0500

    close enough

---
+++ All_Tables.sql
@@ -0,0 +1,109 @@
+create table Seoul_2017 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Seoul_2017
+
+drop table seoul_2017
+
+create table Seoul_2018 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Seoul_2018
+
+create table Seoul_2019 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Seoul_2019
+
+create table Delhi_2017 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Delhi_2017
+
+create table Delhi_2018 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Delhi_2018
+
+create table Delhi_2019 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from Delhi_2019
+
+create table New_York_2017 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from New_York_2017
+
+create table New_York_2018 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from New_York_2018
+
+create table New_York_2019 (
+city varchar(20),
+Year int,
+SO2 dec,
+NO2 dec,
+O3 dec,
+CO dec,
+PM10 dec,
+"pm2.5" dec);
+	
+select * from New_York_2019
